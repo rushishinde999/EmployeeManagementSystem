7e7b34172ec4c0ce011ede8d03064391cd8e9486
rushishinde999 <rushikeshrs2002@gmail.com>
2025-01-27 11:44:51 +0530

    update project

--- EmpMS/Employee-Management-System-BackendAPI-main/Employee-Management-System-BackendAPI-main/src/main/java/com/spring/employee/EmployeeMsApplication.java
+++ EmpMS/Employee-Management-System-BackendAPI-main/Employee-Management-System-BackendAPI-main/src/main/java/com/spring/employee/EmployeeMsApplication.java
@@ -8,6 +8,7 @@ public class EmployeeMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeMsApplication.class, args);
+		System.out.println("App is Started");
 	}
 
 }
